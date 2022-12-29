95788fd6cf03abb5e2c5387dac3be9a087fedf30
Shiran Abir <shirkiabir@gmail.com>
2022-12-29 20:44:14 +0200

    Connect settings form to v-model

--- src/components/settings.vue
+++ src/components/settings.vue
@@ -1,70 +1,77 @@
 <template>
-    <div>
-        <b-button @click="showModal=!showModal">Settings</b-button>
-        <b-modal v-model="showModal" id="settingsMenu" title="Settings" ok-title="Save">
-      <b-form-group
-        id="fieldset-horizontal"
-        label-cols="2"
-        content-cols="9"
-        label="Username:"
-        label-for="input-horizontal"
-      >
-        <b-form-input type="email" placeholder="Enter your email"/>
-      </b-form-group>
-      <b-form-group
-        id="fieldset-horizontal"
-        label-cols="2"
-        content-cols="9"
-        label="Password:"
-        label-for="input-horizontal"
-      >
-        <b-form-input type="password" placeholder="Enter your password"/>
-      </b-form-group>
-      <b-form-group
-        label="Account type:"
-        label-cols="3"
-        v-slot="{ ariaDescribedby }"
-      >
-        <b-form-radio-group
-          class="pt-2"
-          :options="['Normal', 'Google', 'Microsoft']"
-          :aria-describedby="ariaDescribedby"
-        />
-      </b-form-group>
-      <b-form-group
-        label="Read answers:"
-        label-cols="4"
-        label-for="input-horizontal"
-      >
-        <b-form-checkbox
-        id="checkbox-1"
-        name="checkbox-1"
-        value="accepted"
-        unchecked-value="not_accepted"
-        switch
-        size="lg"
-        />
-      </b-form-group>
-    </b-modal>
-    </div>
-  </template>
-  
-  <script lang="ts">
-  export default {
-      data(){
-          return{
-            showModal: false,
-          }
+  <div>
+      <b-button @click="showModal=!showModal">Settings</b-button>
+      <b-modal v-model="showModal" id="settingsMenu" title="Settings" ok-title="Save" @ok="handleOk">
+    <b-form-group
+      id="fieldset-horizontal"
+      label-cols="2"
+      content-cols="9"
+      label="Username:"
+      label-for="input-horizontal"
+    >
+      <b-form-input type="email" v-model="username" placeholder="Enter your email"/>
+    </b-form-group>
+    <b-form-group
+      id="fieldset-horizontal"
+      label-cols="2"
+      content-cols="9"
+      label="Password:"
+      label-for="input-horizontal"
+    >
+      <b-form-input type="password" v-model="password" placeholder="Enter your password"/>
+    </b-form-group>
+    <b-form-group
+      label="Account type:"
+      label-cols="3"
+      v-slot="{ ariaDescribedby }"
+    >
+      <b-form-radio-group
+        class="pt-2"
+        v-model="accountType"
+        :options="['Normal', 'Google', 'Microsoft']"
+        :aria-describedby="ariaDescribedby"
+      />
+    </b-form-group>
+    <b-form-group
+      label="Read answers:"
+      label-cols="4"
+      label-for="input-horizontal"
+    >
+      <b-form-checkbox
+      id="checkbox-1"
+      v-model="shouldRead"
+      name="checkbox-1"
+      switch
+      size="lg"
+      />
+    </b-form-group>
+  </b-modal>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+    data(){
+        return{
+          showModal: false,
+          username: '',
+          password: '',
+          accountType: 'Normal',
+          shouldRead: false
+        }
+    },
+    
+    methods:{
+      handleOk() {
+        console.log('OK!')
       },
-      
-      methods:{
-      },
-      computed:{
-      }
-  }
-  
-  </script>
-  
-  <style>
-  
-  </style>
+    },
+    computed:{
+    }
+}
+
+</script>
+
+<style>
+
+</style>
