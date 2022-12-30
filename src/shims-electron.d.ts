4469013e7c831244328a6b110c94339d6d3e3075
ShiranAbir <106183696+ShiranAbir@users.noreply.github.com>
2022-12-30 11:25:43 +0200

    TypeScript Improvements

--- src/shims-electron.d.ts
+++ src/shims-electron.d.ts
@@ -1,9 +1,11 @@
+export type settingsType = {username: string, password: string, accountType: string, shouldRead: boolean}
+
 // Create a type that should contain all the data we need to expose in the
 // renderer process using `contextBridge`.
 export type ContextBridgeApi = {
     // Declare a `send` function that will return a promise. This promise
     // will contain the result from the main process.
-    send: (channel:string, key:string, value?) => Promise
+    send: (channel:string, key:string, value?: settingsType) => Promise
   }
   
 declare global {
