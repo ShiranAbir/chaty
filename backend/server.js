a0ce9026dbfda669a28d6feb166493dc0b5ac76e
uraid <yairh33@gmail.com>
2022-12-31 19:51:43 +0200

    Improve backend initialization

--- backend/server.js
+++ backend/server.js
@@ -6,14 +6,6 @@ const port = 3000;
 var chatgptFunc = null;
 var chatgptInitFunc = null;
 var speechFunc = null;
-// need to import es module dynamically
-import('./chatgpt.service.mjs').then(chatgpt =>
-  {
-    chatgptFunc = chatgpt.chatgpt
-    chatgptInitFunc = chatgpt.chatgpt_init
-    speechFunc = chatgpt.getSpeechUrl
-  }
-);
 
 app.use(compression())
 app.use(express.json());
@@ -21,19 +13,18 @@ app.use(express.urlencoded({ extended: true }));
 
 app.post("/init", async function (req, res) {
   if (!chatgptFunc){
-    res.send("Backend isn't ready yet!")
-    return
+    const chatgpt = await import('./chatgpt.service.mjs')
+    chatgptFunc = chatgpt.chatgpt
+    chatgptInitFunc = chatgpt.chatgpt_init
+    speechFunc = chatgpt.getSpeechUrl
+
+    console.log("Backend isn't ready yet!")
   }
 
   res.send(await chatgptInitFunc(req.body));
 });
 
 app.post("/ask", async function (req, res) {
-  if (!chatgptFunc){
-    res.send("Backend isn't ready yet!")
-    return
-  }
-
   const question = req.body.question;
   res.send(await chatgptFunc(question));
 });
